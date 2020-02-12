8bf38851d879d052508d2cc5d6e0869e983d08bc
Alexander Sierra <alexandercsierra@gmail.com>
2020-02-12 16:24:17 -0500

    most things working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,78 @@
 const express = require('express');
+const Posts = require('./postDb')
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Posts.get()
+    .then(posts => res.status(200).json(posts))
+    .catch(err=>res.status(500).json({message: "error"}));
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  const {id} = req.params;
+  Posts.getById(id)
+    .then(post => {
+      post ? res.status(200).json({post}) : res.status(404).json({message: 'no post with that id'})
+    .catch(err => res.status(500).json({message: 'server error'}))
+    })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  const {id} = req.params;
+  Posts.remove(id)
+    .then(rem => rem ? res.status(200).json({rem}) : res.status(404).json({message: 'no post with that id'}))
+    .catch(err=>res.status(500).json({message:'server error'}))
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePost, (req, res) => {
+  
+  const {id} = req.params;
+  console.log('id', Number(id))
+  const newPost = req.body;
+  console.log('req.body', req.body.text);
+
+
+  Posts.update(id, newPost)
+    .then(post => {
+      console.log('the post', post);
+      if (post){
+        if(newPost.text){
+          res.status(200).json(post)
+          //never getting here, don't know why
+        } else {
+          res.status(400).json({message: 'need text'})
+        }
+      } else {
+        res.status(404).json({message: 'post at that id does not exist'})
+      }
+    })
+    .catch(err => res.status(500).json({message: 'server error'}))
+
+  
+
 });
+
+
+// router.post('/', (req,res)=>{
+//   const newPost = req.body;
+//   Posts.insert(newPost)
+//     .then(post=>{
+//       post ? 
+//     })
+// })
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
+function validatePost(req, res, next) {
+  console.log('from the validation function', req.body)
+  const theBody = req.body;
+  const keys = Object.keys(theBody);
+  if (keys.length > 0){
+    req.body.text ? next() : res.status(400).json({message: 'missing required text field'})
+  } else {
+    res.status(400).json({message: 'missing post data'})
+  }
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
+const apiRouter = require('./api/api-router');
 const server = express();
+
+server.use(express.json())
+
+server.use('/api', logger, apiRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +12,13 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger (req, res, next) {
+  console.log(`${req.method} request to ${req.originalUrl}`)
+  next();
+}
+
+
+
+
 
 module.exports = server;
